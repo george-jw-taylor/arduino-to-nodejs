048d8aa4ead1e73c6a0d63d31c579d7206814783
george-jw-taylor <109345813+george-jw-taylor@users.noreply.github.com>
2022-07-15 10:23:48 +0100

    Update Takt_Timer.ino

--- Hardware/Takt_Timer.ino
+++ Hardware/Takt_Timer.ino
@@ -1,1 +1,33 @@
+uint8_t VersionNo = 1;
 
+uint8_t switch1 = 2;
+uint8_t switch2 = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.print("Takt Timer - v");
+Serial.print(VersionNo);
+
+pinMode(2, INPUT_PULLUP);
+pinMode(3, INPUT_PULLUP);
+
+attachInterrupt(digitalPinToInterrupt(switch1),trigger_1,RISING);
+attachInterrupt(digitalPinToInterrupt(switch2),trigger_2,RISING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("heartbeat");
+  delay(1000);
+}
+
+void trigger_1() {
+
+  Serial.println("Trigger 1");
+}
+
+void trigger_2() {
+
+  Serial.println("Trigger 2");
+}
